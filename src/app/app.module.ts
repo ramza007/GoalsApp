ad04a092ea33d0d3908a906c0bb285d9e9683307
ramza007 <ramzalvo007@gmail.com>
2018-03-21 17:06:29 +0300

    added parameters for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgProgressModule } from '@ngx-progressbar/core';
 import { NgProgressHttpModule } from '@ngx-progressbar/http';
 
+
 import { AppComponent } from './app.component';
 import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { GoalComponent } from './goal/goal.component';
@@ -13,6 +14,16 @@ import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
 import { AlertsService } from './alert-service/alerts.service';
 import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+// Defining routes
+const routes: Routes = [
+  { path: "goals", component: GoalComponent },
+  { path: "about", component: AboutComponent },
+  { path: "", redirectTo: "/goals", pathMatch: "full" },
+  { path: '**', component: NotFoundComponent }
+]
+import { RoutingModule } from './routing/routing.module'
 
 
 @NgModule({
@@ -23,15 +34,20 @@ import { AboutComponent } from './about/about.component';
     StrikethroughDirective,
     DateCountPipe,
     GoalFormComponent,
-    AboutComponent
+    AboutComponent,
+    NotFoundComponent
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
+    RoutingModule,
     HttpClientModule,
-    NgProgressModule.forRoot(),// normal progress bar
-    NgProgressHttpModule // progress bar to load http requests
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule,
+
   ],
+  
   providers: [AlertsService], // Add service to providers
   bootstrap: [AppComponent]
 })
