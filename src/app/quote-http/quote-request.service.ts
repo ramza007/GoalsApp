6bad200d39287d6cf94ccf7e58cabe1b1c170e51
ramza007 <ramzalvo007@gmail.com>
2018-03-22 15:48:05 +0300

    add goal button

--- src/app/quote-http/quote-request.service.ts
+++ src/app/quote-http/quote-request.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { environment } from '../../environments/environment'
-import { Quote } from '../quote-class/quote'
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Quote } from '../quote-class/quote';
 
 
 @Injectable()
@@ -10,32 +10,32 @@ export class QuoteRequestService {
   quote: Quote;
 
   constructor(private http: HttpClient) {
-    this.quote = new Quote("", "");
+    this.quote = new Quote('', '');
   }
 
   quoteRequest() {
 
     interface ApiResponse {
       quote: string;
-      author: string
+      author: string;
 
     }
     let promise = new Promise((resolve, reject) => {
       this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response => {
 
-        this.quote.quote = response.quote
-        this.quote.author = response.author
+        this.quote.quote = response.quote;
+        this.quote.author = response.author;
 
-        resolve()
+        resolve();
       },
         error => {
-          this.quote.quote = "Never, never, never give up."
-          this.quote.author = "winston churchill"
-          reject(error)
+          this.quote.quote = 'Never, never, never give up.';
+          this.quote.author = 'winston churchill';
+          reject(error);
         }
-      )
-    })
+      );
+    });
 
-    return promise
+    return promise;
   }
 }
